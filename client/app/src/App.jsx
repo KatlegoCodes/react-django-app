d13e9ad9b900048988cc495bd78de93f951a43bf
Frost <katlegcodes@gmail.com>
2026-02-18 17:52:26 +0200

    Added home page and styled it using tailwindcss

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="bg-gray-900 min-h-screen flex flex-col justify-center items-center">
+      <div className="text-gray-200 flex flex-col items-center">
+        <h1 className="text-5xl font-bold text-center">Book Website</h1>
 
-export default App
+        <div className="mt-5 flex justify-center">
+          <input
+            type="text"
+            placeholder="Book title..."
+            className="border-2 p-1 mx-2 rounded-lg hover:border-indigo-500 transition"
+          />
+          <input
+            type="text"
+            placeholder="Release date"
+            className="border-2 p-1 mx-2 rounded-lg hover:border-indigo-500 transition"
+          />
+          <button className="bg-indigo-700 px-4 py-1.5 rounded-lg hover:scale-105 transition transition-300 active:bg-indigo-400">
+            Add book
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
